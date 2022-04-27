54b7d8a843f42dcc8c8318ef99f014e265c1be4e
Edwin Żurawik <ezurawik35@gmail.com>
2022-04-27 22:01:47 +0200

    feat: using flatty version 0.0.4

--- app/app.py
+++ app/app.py
@@ -1,9 +1,13 @@
 import multiprocessing
 import os
 
-from dotenv import load_dotenv
-from flask import Flask, abort, request
 import flatty
+from dotenv import load_dotenv
+from flask import (
+    Flask,
+    abort,
+    request,
+)
 
 load_dotenv()
 
@@ -20,19 +24,22 @@ def get_offers():
     content_type = request.headers.get("Content-Type")
     if content_type == "application/json":
         content = request.get_json()
-        print(os.environ["FLASK_API_KEY"])
         if (
             "api_key" in content.keys()
             and content["api_key"] == os.environ["FLASK_API_KEY"]
         ):
-            city = content.get('city')
+            city = content.get("city")
             if city:
-                print(city)
-                get_offers_work = multiprocessing.Process(target=flatty.get_offers_for_city,
-                                                    args=(city,))
-                get_offers_work.start()
+                multiprocessing.Process(
+                    target=scrape_and_save_offers, args=(city,)
+                ).start()
                 return "Processing request...", 201
             return abort(400, "No city specified")
         else:
             abort(401, "Not Authenticated!")
     abort(400, "Content-Type not supported!")
+
+
+def scrape_and_save_offers(city: str) -> None:
+    offers_list = flatty.get_offers_for_city(city)
+    flatty.update_offers(offers_list)
